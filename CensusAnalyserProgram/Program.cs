271754d2a9de43fea6289de37ee936ebef230bc7
“Salil48” <salilchharia@gmail.com>
2023-01-27 21:06:09 +0530

    Welcome to Indian States Census Analyser Problem

---
+++ CensusAnalyserProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CensusAnalyserProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian States Census Analyser program");
+        }
+    }
+}
